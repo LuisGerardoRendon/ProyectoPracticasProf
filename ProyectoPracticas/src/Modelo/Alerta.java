b2a10fad6599eb90715065dedd1276cf3c4a1974
rendo <rendo@DESKTOP-0FGPCGK>
2020-06-04 16:45:19 -0500

    alertas

--- ProyectoPracticas/src/Modelo/Alerta.java
+++ ProyectoPracticas/src/Modelo/Alerta.java
@@ -6,23 +6,8 @@
 
 package Modelo;
 import javafx.scene.control.Alert;
-/**
- * 
- * @author Sammy Guergachi <sguergachi at gmail.com>
- */
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+//ESTA CLASE SIRVE PARA MANDAR MENSAJES A LA PANTALLA
 
 /**
  *
